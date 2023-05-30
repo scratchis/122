73f882dd1749256b7909df9dede4d9ba19e0c466
scratchis <103792439+scratchis@users.noreply.github.com>
2023-05-30 19:36:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup(){
+
+canvas=createCanvas(640,480);
+    canvas.position(110,250);
+}
+function draw(){
+    circle(60, 20, 40);
+    circle(50, 160, 70);
+    rect(430, 720, 55, 55, 20, 15, 10, 5);
+    rect(570, 320, 20, 10, 20, 15, 10, 5);
+    rect(630, 120, 55, 55);
+    rect(130, 660, 55, 20);
+    ellipse(556, 146, 55, 30);
+    ellipse(480, 180, 55, 55);
+}
